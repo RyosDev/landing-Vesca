a7c83aab3960a8ef4870fd1080728b5d5dc965c8
Ry0s <eliastello.dev@gmail.com>
2024-04-21 10:39:27 -0300

    Landing page of VESCA

---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        lightBlue: "#595B93",
+        strongBlue: "#1B1D6A",
+        titlleColor: "#292B79",
+        textColor: "#9EA0D6",
+        strongText: "#696CC9",
+      },
+    },
+  },
+  plugins: [],
+};
